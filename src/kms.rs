ef2e356992fa3e4f1df7b202aa8f7e729dc133c5
rink1969 <rink1969@gmail.com>
2021-04-22 16:14:46 +0800

    use grpc status code as error

--- src/kms.rs
+++ src/kms.rs
@@ -22,6 +22,7 @@ use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::types::ToSql;
 use rusqlite::{Error, Result};
 use std::vec::Vec;
+use tonic::Status;
 
 const PASSWORD_SALT: &str = "Matthew 5-13";
 pub const CONFIG_TYPE: &str = "sm";
@@ -123,7 +124,7 @@ impl KMS {
         pubkey: Vec<u8>,
         privkey: Vec<u8>,
         description: String,
-    ) -> Result<u64, String> {
+    ) -> Result<u64, Status> {
         let conn = self.pool.get().unwrap();
 
         let ret = conn.execute(
@@ -137,30 +138,29 @@ impl KMS {
 
         if let Err(e) = ret {
             let err_str = format!("insert key failed: {:?}", e);
-            Err(err_str)
+            Err(Status::aborted(err_str))
         } else {
             let latest_id = conn.last_insert_rowid();
             Ok(latest_id as u64)
         }
     }
 
-    pub fn generate_key_pair(&self, description: String) -> Result<(u64, Vec<u8>), String> {
+    pub fn generate_key_pair(&self, description: String) -> Result<(u64, Vec<u8>), Status> {
         let (pk, sk) = generate_keypair();
         let encrypted_sk = encrypt(&self.password, sk);
         self.insert_account(pk.clone(), encrypted_sk, description)
-            .map_or_else(Err, |key_id| {
+            .map(|key_id| {
                 let address = pk2address(&pk);
-                Ok((key_id, address))
+                (key_id, address)
             })
     }
 
-    pub fn hash_date(&self, data: &[u8]) -> Result<Vec<u8>, String> {
-        let hash = hash_data(data);
-        Ok(hash)
+    pub fn hash_date(&self, data: &[u8]) -> Vec<u8> {
+        hash_data(data)
     }
 
-    pub fn verify_data_hash(&self, data: Vec<u8>, hash: Vec<u8>) -> Result<bool, String> {
-        Ok(verify_data_hash(data, hash))
+    pub fn verify_data_hash(&self, data: Vec<u8>, hash: Vec<u8>) -> bool {
+        verify_data_hash(data, hash)
     }
 
     fn get_account(&self, key_id: u64) -> Result<(Vec<u8>, Vec<u8>)> {
@@ -179,24 +179,26 @@ impl KMS {
         }
     }
 
-    pub fn sign_message(&self, key_id: u64, msg: Vec<u8>) -> Result<Vec<u8>, String> {
+    pub fn sign_message(&self, key_id: u64, msg: Vec<u8>) -> Result<Vec<u8>, Status> {
         if let Ok((pubkey, privkey)) = self.get_account(key_id) {
             if let Some(signature) = sign_message(pubkey, privkey, msg) {
                 Ok(signature)
             } else {
-                Err("Sign msg failed".to_owned())
+                Err(Status::invalid_argument("Sign msg failed".to_owned()))
             }
         } else {
-            Err("Can't find key id!".to_owned())
+            Err(Status::aborted("Can't find key id!".to_owned()))
         }
     }
 
-    pub fn recover_signature(&self, msg: Vec<u8>, signature: Vec<u8>) -> Result<Vec<u8>, String> {
+    pub fn recover_signature(&self, msg: Vec<u8>, signature: Vec<u8>) -> Result<Vec<u8>, Status> {
         if let Some(pubkey) = recover_signature(msg, signature) {
             let address = pk2address(&pubkey);
             Ok(address)
         } else {
-            Err("Recover signature failed".to_owned())
+            Err(Status::invalid_argument(
+                "Recover signature failed".to_owned(),
+            ))
         }
     }
 }
